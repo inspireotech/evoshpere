5e04877fae6f6f22a6e4f82655a30325fe2ec2a3
inspireotech <inspireotech@gmail.com>
2024-08-06 01:05:24 +0500

    first ce

--- pages/services.js
+++ pages/services.js
@@ -6,7 +6,7 @@ const services = () => {
   return (
     <>
       <Head>
-        <title>Team - 7Creative</title>
+        <title>Team - Evoshpere Marketing</title>
       </Head>
       <Services />
     </>
